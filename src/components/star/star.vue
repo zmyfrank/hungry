d637bf70f9a036243483b0a81f3e72c5393c147a
frank <616180981@qq.com>
2017-09-05 17:49:20 +0800

    1.star组件基本完成

--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -1,11 +1,90 @@
 <template>
-
+    <div class="star" :class="starType">
+        <span v-for="itemClass in itemClasses " class="star-item" :class="itemClass"></span>
+    </div>
 </template>
 
 <script>
-  export default {}
+  const LENGTH = 5
+  const CLS_ON = 'on'
+  const CLS_HALF = 'half'
+  const CLS_OFF = 'off'
+  export default {
+    props: {
+      size: {
+        type: Number
+      },
+      score: {
+        type: Number
+      }
+    },
+    computed: {
+      starType () {
+        return 'star-' + this.size
+      },
+      itemClass () {
+        let result = []
+        let score = Math.floor(this.score * 2) / 2
+        let hasDecimal = score % 1 !== 0
+        for (let i = 0; i < Math.floor(score); i++) {
+          result.push(CLS_ON)
+        }
+        if (hasDecimal) {
+          result.push(CLS_HALF)
+        }
+        while (result.length < LENGTH) {
+          result.push(CLS_OFF)
+        }
+      }
+    }
+  }
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
-
+    @import "../../common/scss/mixin";
+    .star{
+        font-size:0;
+        .star-item{
+            display:inline-block;
+            background-repeat:no-repeat;
+        }
+        &.star-48{
+            .star-item{
+                &:nth-child(n+2){
+                    margin-left:22px;
+                }
+                width:20px;
+                height:20px;
+                background-size:20px 20px;
+                &.on{
+                    @include bg-img('star48_on')
+                }
+                &.half{
+                    @include bg-img('star48_half')
+                }
+                &.off{
+                    @include bg-img('star48_off')
+                }
+            }
+        }
+        &.star-36{
+            .star-item{
+                &:nth-child(n+2){
+                    margin-left:22px;
+                }
+                width:20px;
+                height:20px;
+                background-size:20px 20px;
+                &.on{
+                    @include bg-img('star36_on')
+                }
+                &.half{
+                    @include bg-img('star36_half')
+                }
+                &.off{
+                    @include bg-img('star36_off')
+                }
+            }
+        }
+    }
 </style>
