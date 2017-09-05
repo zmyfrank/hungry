68e15d659985a08f217cb4a628dc2b92aeee2c47
frank <616180981@qq.com>
2017-09-05 17:13:07 +0800

    1.做到了star组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,12 +17,26 @@
                     </span>
                 </div>
             </div>
-            <div class="support-content" v-if="seller.supports">
-                <span class="count">{{seller.supports.length}}个</span>
-                <i class="icon-keyboard_arrow_right"></i>
+            <div class="support-content" v-if="seller.supports" @click="detailShow">
+                <span class="count">{{seller.supports.length}}个</span> <i class="icon-keyboard_arrow_right"></i>
             </div>
         </div>
-        <div class="bulletin-wrapper"></div>
+        <div class="bulletin-wrapper" @click="detailShow">
+            <span class="bulletin-wrapper-brand"></span><span class="bulletin-wrapper-main">{{seller.bulletin}}</span> <i class="icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background-img">
+            <img :src="seller.avatar" width="100%" height="100%">
+        </div>
+        <div class="detail" v-show="showDetail">
+            <div class="detail-wrapper clearfix">
+                <div class="detail-main">
+
+                </div>
+            </div>
+            <div class="detail-footer" @click="detailShow">
+                <i class="icon-close"></i>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -33,8 +47,18 @@
         type: Object
       }
     },
+    data () {
+      return {
+        showDetail: false
+      }
+    },
     created () {
       this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+    },
+    methods: {
+      detailShow () {
+        this.showDetail = !this.showDetail
+      }
     }
   }
 </script>
@@ -42,8 +66,9 @@
 <style lang="scss" rel="stylesheet/scss" scoped>
     @import "../../common/scss/mixin";
     .header{
+        position:relative;
         color:#fff;
-        background-color:#999;
+        background-color:rgba(7, 17, 27, 0.5);
         .content-wrapper{
             padding:24px 12px 18px 24px;
             position:relative;
@@ -129,5 +154,70 @@
                 }
             }
         }
+        .bulletin-wrapper{
+            position:relative;
+            padding:0 22px 0 12px;
+            height:28px;
+            line-height:28px;
+            background-color:rgba(7, 17, 27, 0.2);
+            text-overflow:ellipsis;
+            white-space:nowrap;
+            overflow:hidden;
+            .bulletin-wrapper-brand{
+                display:inline-block;
+                vertical-align:top;
+                margin-top:8px;
+                width:22px;
+                height:12px;
+                @include bg-img('bulletin');
+                background-size:22px 12px;
+                background-repeat:no-repeat;
+            }
+            .bulletin-wrapper-main{
+                vertical-align:top;
+                margin:0 4px;
+                font-size:10px;
+            }
+            .icon-keyboard_arrow_right{
+                position:absolute;
+                font-size:10px;
+                top:8px;
+                right:12px;
+            }
+        }
+        .background-img{
+            position:absolute;
+            top:0;
+            left:0;
+            z-index:-1;
+            width:100%;
+            height:100%;
+            filter:blur(10px);
+        }
+        .detail{
+            position:fixed;
+            top:0;
+            left:0;
+            width:100%;
+            height:100%;
+            overflow:auto;
+            background-color:rgba(7, 17, 27, 0.8);
+            z-index:100;
+            .detail-wrapper{
+                min-height:100%;
+                .detail-main{
+                    margin-top:64px;
+                    padding:0 72px 64px;
+                }
+            }
+            .detail-footer{
+                position:relative;
+                width:32px;
+                height:32px;
+                margin:-64px auto 0 auto;
+                clear:both;
+                font-size:32px;
+            }
+        }
     }
-</style>
+</style>`
